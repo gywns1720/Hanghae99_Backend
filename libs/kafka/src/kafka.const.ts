9f67cecc518f38a982f292e238618c5f786bc40c
gywns1720 <93173809+gywns1720@users.noreply.github.com>
2025-07-18 01:08:52 +0900

    [Update] Kafkajs 라이브러리 설치
    1. Service , Module 생성 카프카용

---
+++ libs/kafka/src/kafka.const.ts
@@ -0,0 +1,29 @@
+import * as process from 'node:process';
+
+const KafkaConst = {
+  Inject: {
+    Group: 'kafka.service.group_id',
+    Client: 'kafka.service.client_id',
+  } as const,
+  Brokers: ['localhost:9092', 'localhost:9093', 'localhost:9094'],
+  Groups: {
+    User: {
+      Id: 'user-service-group',
+      ClientId: `user_server_${process.pid}`,
+    },
+    Order: {
+      Id: 'order-service-group',
+      ClientId: `order_server_${process.pid}`,
+    },
+    Log: {
+      Id: 'log-service-group',
+      ClientId: `log_server_${process.pid}`,
+    },
+  } as const,
+  Topics: {
+    USER_EVENT: 'user-event',
+    ORDER_EVENT: 'order-event',
+    LOG_EVENT: 'log-event',
+  } as const,
+};
+export default KafkaConst;
--- libs/kafka/src/kafka.service.ts
+++ libs/kafka/src/kafka.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import KafkaConst from '@app/kafka/kafka.const';
+import { Consumer, Kafka, Producer } from 'kafkajs';
 
 @Injectable()
-export class KafkaService {}
+export class KafkaService implements OnModuleInit, OnModuleDestroy {
+  protected kafka: Kafka;
+  protected producer: Producer;
+  protected consumer: Consumer;
+  constructor(
+    @Inject(KafkaConst.Inject.Group) protected readonly groupId: string,
+    @Inject(KafkaConst.Inject.Client) protected readonly clientId: string,
+  ) {
+    this.kafka = new Kafka({
+      clientId,
+      brokers: KafkaConst.Brokers,
+    });
+
+    this.producer = this.kafka.producer();
+    this.consumer = this.kafka.consumer({ groupId });
+  }
+
+  //#region [Implements]
+  async onModuleDestroy() {
+    await this.producer.disconnect();
+    await this.consumer.disconnect();
+  }
+
+  async onModuleInit() {
+    await this.producer.connect();
+    await this.consumer.connect();
+  }
+  //#endregion
+
+  async sendMessage(topic: string, message: any) {
+    await this.producer.send({
+      topic,
+      messages: [
+        {
+          value: JSON.stringify(message),
+          timestamp: Date.now().toString(),
+        },
+      ],
+    });
+  }
+
+  async subscribeToTopic(topic: string, callback: (message: any) => void) {
+    await this.consumer.subscribe({ topic, fromBeginning: false });
+
+    await this.consumer.run({
+      eachMessage: async ({ message }) => {
+        const messageValue = message.value?.toString();
+        if (messageValue) {
+          const parsedMessage = JSON.parse(messageValue);
+          callback(parsedMessage);
+        }
+      },
+    });
+  }
+}
